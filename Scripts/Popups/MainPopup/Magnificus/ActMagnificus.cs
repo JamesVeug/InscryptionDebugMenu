713add4d5b686490fae692777a7f78cf148057be
jamesveug <james.veugelaers@outlook.co.nz>
2023-06-09 21:41:43 +1200

    Added light Magnificus Mod support

--- Scripts/Popups/MainPopup/Magnificus/ActMagnificus.cs
+++ Scripts/Popups/MainPopup/Magnificus/ActMagnificus.cs
@@ -1,5 +1,7 @@
 ﻿using BepInEx.Logging;
 using DebugMenu.Scripts.Acts;
+using DebugMenu.Scripts.Utils;
+using DiskCardGame;
 
 namespace DebugMenu.Scripts.Magnificus;
 
@@ -7,6 +9,7 @@ public class ActMagnificus : BaseAct
 {
 	public ActMagnificus(DebugWindow window) : base(window)
 	{
+		m_cardBattleSequence = new CardBattleSequence(window);
 	}
 
 	public override void Update()
@@ -17,12 +20,50 @@ public class ActMagnificus : BaseAct
 	public override void OnGUI()
 	{
 		Window.LabelHeader("Magnificus Act");
-		Window.Label("Support not started");
+		OnGUICurrentNode();
 	}
 
 	public override void OnGUIMinimal()
 	{
-		OnGUI();
+		OnGUICurrentNode();
+	}
+	
+	private void OnGUICurrentNode()
+	{
+		MagnificusGameFlowManager gameFlowManager = Singleton<MagnificusGameFlowManager>.m_Instance;
+		if (gameFlowManager == null)
+		{
+			return;
+		}
+
+		Window.LabelHeader(gameFlowManager.CurrentGameState.ToString());
+		switch (gameFlowManager.CurrentGameState)
+		{
+			case GameState.CardBattle:
+				m_cardBattleSequence.OnGUI();
+				break;
+			case GameState.Map:
+				// Show map related buttons
+				OnGUIMap();
+				break;
+			case GameState.FirstPerson3D:
+				break;
+			case GameState.SpecialCardSequence:
+				SpecialNodeData nodeWithId = Helpers.LastSpecialNodeData;
+				Type nodeType = nodeWithId.GetType();
+				Window.Label("Unhandled node type");
+				Window.Label(nodeType.FullName);
+				break;
+			default:
+				Window.Label("Unhandled GameFlowState:");
+				Window.Label(gameFlowManager.CurrentGameState.ToString());
+				break;
+		}
+	}
+
+	private void OnGUIMap()
+	{
+		Window.Label("Support not started");
 	}
 
 	public override void Restart()
